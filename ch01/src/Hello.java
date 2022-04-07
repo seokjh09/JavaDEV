5c036b6950a95e5d3f8be965a509bfd2864cbd5d
seokjh09 <juhee1757@gmail.com>
2022-04-07 17:56:30 +0900

    4.7 commit

--- ch01/src/Hello.java
+++ ch01/src/Hello.java
@@ -2,6 +2,15 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		// ctrl + shift + L 단축기 전체 목록
+		// ctrl + D 한 줄 삭제
+		// ctrl + art + down 행 단위 복사
+		// alt + shift + A 멀티컬럼 편집
+		// alt + up, down 행 단위 이동
+		// ctrl + i 자동 들여쓰기
+		// ctrl + / 주석(토글)
+		// ctrl + space 자동완성
+		System.out.println();
 		System.out.println("Hello, world.");
 	}
 }
